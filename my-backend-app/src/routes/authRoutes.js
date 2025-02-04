56baf7ba4b0785bdbbdc83612d0d04a7ad8d2981
Kundan Kumar <kun9577@gmail.com>
2025-02-04 20:52:38 +0530

    authentication implimented

--- my-backend-app/src/routes/authRoutes.js
+++ my-backend-app/src/routes/authRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { register, login, logout, getProfile, forgotPassword, resetPassword } from '../controllers/authController.js';
-import { isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.get('/profile', isAuthenticated, getProfile);
+router.get('/profile', getProfile);
 
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
