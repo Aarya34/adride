ded052605bb51a79f3f38c9d0f5423e3d7b0542a
Kundan Kumar <kun9577@gmail.com>
2025-02-11 14:09:52 +0530

    Removed Authentication

--- my-backend-app/src/routes/adRoutes.js
+++ my-backend-app/src/routes/adRoutes.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import { isAuthenticated, hasRole } from '../middlewares/authMiddleware.js';
 import { createAd, getAds, getMyAds, editAd, deleteAd } from '../controllers/adController.js';
 import upload from '../middlewares/upload.js';
 
 const router = express.Router();
 
-router.post('/create', isAuthenticated, hasRole('advertiser'), upload.single('image'), createAd);
-
+router.post('/create', upload.single('image'), createAd);
 router.get('/', getAds);
-
-router.get('/my-ads', isAuthenticated, hasRole('advertiser'), getMyAds);
-
-router.put('/edit/:id', isAuthenticated, hasRole('advertiser'), upload.single('image'), editAd);
-
-router.delete('/delete/:id', isAuthenticated, hasRole('advertiser'), deleteAd);
+router.get('/my-ads', getMyAds);
+router.put('/edit/:id', upload.single('image'), editAd);
+router.delete('/delete/:id', deleteAd);
 
 export default router;
